14012aa4f1c02cdc375126f17be1a15a9339ffa5
bzt <98985738+BrunoZenatti@users.noreply.github.com>
2022-08-12 11:31:36 -0300

    Add files via upload

---
+++ CrudUsuario/src/main/java/br/ufscar/dc/dsw/domain/Agencia.java
@@ -0,0 +1,55 @@
+package br.ufscar.dc.dsw.domain;
+
+public class Agencia {
+	private Long id;
+	private String email;
+	private String senha;
+	private String cnpj;
+	private String nome;
+	private String descricao;
+	private String role;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
