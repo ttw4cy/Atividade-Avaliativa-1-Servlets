841d997030d01fd2c95e223a0d7d6eda95ae0328
lvizfernando <52456458+lvizfernando@users.noreply.github.com>
2022-08-12 14:14:24 -0300

    Update UsuarioDAO.java

--- CrudUsuario/src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
+++ CrudUsuario/src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
@@ -1,7 +1,150 @@
 package br.ufscar.dc.dsw.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufscar.dc.dsw.domain.Usuario;
 
 public class UsuarioDAO extends GenericDAO {
+	public void insert(Usuario usuario){
+        String sql = "INSERT INTO usuario (username, password, cpf, nome, telefone, sexo, datanasc, role) VALUES (?, ?, ?, ?) ";
 
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, usuario.getUsername());
+            statement.setString(2, usuario.getPassword());
+            statement.setString(3, usuario.getCpf());
+            statement.setString(5, usuario.getNome());
+            statement.setString(6, usuario.getTelefone());
+            statement.setString(7, usuario.getSexo());
+            statement.setString(8, usuario.getDatanasc());
+            statement.setString(9, usuario.getRole());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+	
+	public List<Usuario> getAll() {
+
+        List<Usuario> listaUsuarios = new ArrayList<>();
+
+        String sql = "SELECT * from Usuario";
+
+        try {
+            Connection conn = this.getConnection();
+            Statement statement = conn.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String cpf = resultSet.getString("cpf");
+                String nome = resultSet.getString("nome");
+                String telefone = resultSet.getString("telefone");
+                String sexo = resultSet.getString("sexo");
+                String datanasc = resultSet.getString("datanasc");
+                String role = resultSet.getString("role");
+                Usuario usuario = new Usuario(id, username, password, cpf, nome , telefone, sexo, datanasc, role);
+                listaUsuarios.add(usuario);
+            }
+
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return usuario;
+    }
+
+    public void update(Usuario usuario) {
+        String sql = "UPDATE usuario SET username = ?, password = ?, cpf = ?, nome = ?, telefone = ?, sexo = ?, datanasc = ?, role = ?, WHERE id = ?";
+
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, usuario.getUsername());
+            statement.setString(2, usuario.getPassword());
+            statement.setString(3, usuario.getCpf());
+            statement.setString(5, usuario.getNome());
+            statement.setString(6, usuario.getTelefone());
+            statement.setString(7, usuario.getSexo());
+            statement.setString(8, usuario.getDatanasc());
+            statement.setString(9, usuario.getRole());
+            statement.setLong(10, usuario.getId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(Usuario usuario){
+        String sql = "DELETE FROM usuario where id = ?";
+
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setLong(1, usuario.getId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Usuario getbyLogin(String login) {
+        Usuario usuario = null;
+
+        String sql = "SELECT * from Usuario WHERE login = ?";
+
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setString(1, login);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+            	long id = resultSet.getLong("id");
+            	String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String cpf = resultSet.getString("cpf");
+                String nome = resultSet.getString("nome");
+                String telefone = resultSet.getString("telefone");
+                String sexo = resultSet.getString("sexo");
+                String datanasc = resultSet.getString("datanasc");
+                String role = resultSet.getString("role");
+
+               usuario = new Usuario(id, username, password, cpf, nome , telefone, sexo, datanasc, role);
+            }
+
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return usuario;
+    }
+    
 }
