2a372ca3959c3dd728202333d9d51f5edec4edb8
bzt <98985738+BrunoZenatti@users.noreply.github.com>
2022-08-12 10:57:53 -0300

    Update create.sql

--- CrudUsuario/db/create.sql
+++ CrudUsuario/db/create.sql
@@ -16,3 +16,14 @@ CREATE TABLE Usuario(
     role VARCHAR(20) NOT NULL, 
     PRIMARY KEY (id)
 );
+
+CREATE TABLE Agencia{
+    id BIGINT NOT NULL auto_increment,
+    email VARCHAR(64) NOT NULL UNIQUE,
+    senha VARCHAR(64) NOT NULL,
+    cnpj VARCHAR(15) NOT NULL UNIQUE,
+    nome VARCHAR(64) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL, 
+    PRIMARY KEY (id)
+};
